581f17925ece29cfcafb103190088ddc71cda945
정아영 <monayoung0323@naver.com>
2023-06-09 10:50:17 +0900

    add: activity_login.xml 진행중

--- app/src/main/java/com/example/eighteenadult/BeginningActivity.kt
+++ app/src/main/java/com/example/eighteenadult/BeginningActivity.kt
@@ -1,11 +1,23 @@
 package com.example.eighteenadult
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class BeginningActivity : AppCompatActivity() {
+
+    lateinit var btn_start : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_beginning)
+
+        btn_start = findViewById(R.id.btn_start)
+        btn_start.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/eighteenadult/SplashActivity.kt
+++ app/src/main/java/com/example/eighteenadult/SplashActivity.kt
@@ -1,11 +1,15 @@
 package com.example.eighteenadult
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        val intent = Intent(applicationContext, BeginningActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
